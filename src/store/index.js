96e331c035950a9e985c4cba5611830837fb41f9
Shimshon Zacken <shimshonzacken@shimshons-mbp.lan>
2019-01-24 08:41:23 +0100

    initial state moved out

--- src/store/index.js
+++ src/store/index.js
@@ -2,14 +2,16 @@ import { createStore } from 'redux';
 import reducers from '../reducers';
 import { units } from '../services/openweathermap/units';
 
-const store = createStore(reducers, {
+const initialState = {
     weather: {
         currentWeather: []
     },
     currentTempType: units.celsius,
     favorites: [],
     historyList: []
-});
+};
+
+const store = createStore(reducers, initialState);
 
 window.STORE = store;
 
