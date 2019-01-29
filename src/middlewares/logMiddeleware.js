808ece74f168218672f9fb69ad4d7f9c8b882d72
Shimshon Zacken <shimshonzacken@shimshons-mbp.lan>
2019-01-29 10:51:42 +0100

    theme added

--- src/middlewares/logMiddeleware.js
+++ src/middlewares/logMiddeleware.js
@@ -1,6 +1,6 @@
 export const logMiddeleware = ({ dispatch, getState }) => next => action => {
 
-    console.log('Redux Logger', action.type, getState());
-    
-    return next(action);
+    console.log('Redux Logger - before', action.type, getState());
+    next(action);
+    console.log('Redux Logger - after', action.type, getState());
 };
