9bff198f8c8a31faf4b279ba3c9be3e25734ace5
shimizacken <shimizacken@gmail.com>
2019-11-29 23:00:10 +0100

    upgrades

--- src/components/weather/searchHistory/index.jsx
+++ src/components/weather/searchHistory/index.jsx
@@ -1,26 +1,28 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import List from './list';
 import style from './style.scss';
 
-class SearchHistoryContainer extends Component {
-  render() {
-    if (!this.props.searchHistory) {
-      return null;
-    }
-
-    return (
-      <div className={style.searchHistoryPanel}>
-        <h4>History ({this.props.searchHistory.length})</h4>
-        <List list={this.props.searchHistory.reverse()} />
-      </div>
-    );
+export const SearchHistoryContainer = () => {
+  if (!this.props.searchHistory) {
+    return null;
   }
-}
+
+  return (
+    <div className={style.searchHistoryPanel}>
+      <h4>History ({this.props.searchHistory.length})</h4>
+      <List list={this.props.searchHistory.reverse()} />
+    </div>
+  );
+};
 
 const mapStateToProps = state => ({
   searchHistory: state.searchHistory
 });
 
 export default connect(mapStateToProps)(SearchHistoryContainer);
+
+SearchHistoryContainer.propTypes = {
+  searchHistory: PropTypes.array
+};
