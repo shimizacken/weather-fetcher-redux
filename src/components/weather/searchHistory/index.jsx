9676f6d0e5f20b51dfd0f6fec91f8f3b4cc3da8c
shimizacken <shimizacken@gmail.com>
2019-11-29 23:01:51 +0100

    SearchHistoryContainer refactored

--- src/components/weather/searchHistory/index.jsx
+++ src/components/weather/searchHistory/index.jsx
@@ -1,27 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 import List from './list';
 import style from './style.scss';
 
 export const SearchHistoryContainer = () => {
-  if (!this.props.searchHistory) {
+  const searchHistory = useSelector(state => state.searchHistory);
+
+  if (!searchHistory) {
     return null;
   }
 
   return (
     <div className={style.searchHistoryPanel}>
-      <h4>History ({this.props.searchHistory.length})</h4>
-      <List list={this.props.searchHistory.reverse()} />
+      <h4>History ({searchHistory.length})</h4>
+      <List list={searchHistory.reverse()} />
     </div>
   );
 };
-
-const mapStateToProps = state => ({
-  searchHistory: state.searchHistory
-});
-
-export default connect(mapStateToProps)(SearchHistoryContainer);
 
 SearchHistoryContainer.propTypes = {
   searchHistory: PropTypes.array
