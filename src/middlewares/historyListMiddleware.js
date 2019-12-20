352d66d2920f2d4176f0eb778555fb95beb48371
shimizacken <shimizacken@gmail.com>
2019-12-20 13:58:51 +0100

    cleanup

--- src/middlewares/historyListMiddleware.js
+++ src/middlewares/historyListMiddleware.js
@@ -5,8 +5,6 @@ import {
 } from '../constants';
 
 export const historyListMiddleware = ({ dispatch, getState }) => next => action => {
-  // historyListMiddleware
-
   if (action.type === END_ADD_TO_SEARCH_HISTORY || action.type === END_REMOVE_ITEM_FROM_HISTORY) {
     window.localStorage.setItem('_history_list_', JSON.stringify(getState().searchHistory));
 
--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,6 +1,5 @@
 import thunk from 'redux-thunk';
 import { historyListMiddleware } from './historyListMiddleware';
 import { themeTypeMiddleware } from '../features/theme/state/themeTypeMiddleware';
-import { logMiddleware } from './logMiddleware';
 
 export const middlewares = [thunk, historyListMiddleware, themeTypeMiddleware];
