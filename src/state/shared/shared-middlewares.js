ee008758768aa5b9b5d2393f161b690ad28ecbbd
shimizacken <shimizacken@gmail.com>
2020-01-11 18:36:53 +0100

    eslinted

--- src/state/shared/shared-middlewares.js
+++ src/state/shared/shared-middlewares.js
@@ -2,8 +2,4 @@ import thunk from 'redux-thunk';
 import { historyListMiddleware } from 'app/features/searchHistory';
 import { themeTypeMiddleware } from 'app/features/theme/state/themeTypeMiddleware';
 
-export const middlewares = [
-    thunk,
-    historyListMiddleware,
-    themeTypeMiddleware
-];
+export const middlewares = [thunk, historyListMiddleware, themeTypeMiddleware];
