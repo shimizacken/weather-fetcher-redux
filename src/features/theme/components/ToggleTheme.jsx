d1688d02b62d8adbaf61f057153e743448a09db8
shimizacken <shimizacken@gmail.com>
2020-01-12 12:03:52 +0100

    conflicts fixes

--- src/features/theme/components/ToggleTheme.jsx
+++ src/features/theme/components/ToggleTheme.jsx
@@ -1,14 +1,13 @@
 import React from 'React';
-import { useDispatch } from 'react-redux';
-import { RadioButton } from '../../../../components/common';
-import { ThemeTypes } from '../../bll/themeTypes';
-import { getPersistedTheme } from '../../bll/getPersistedTheme';
-import { toggleTheme, themeChanged } from '../../state/actions';
-import styles from './styles.scss';
+import { useDispatch, useSelector } from 'react-redux';
+import { RadioButton } from '../../../components/common';
+import { ThemeTypes } from '../bll/themeTypes';
+import { toggleTheme, themeChanged } from '../state/actions';
+import styles from './ToggleTheme.scss';
 
 export const ToggleTheme = () => {
   const dispatch = useDispatch();
-  const defaultThemeType = getPersistedTheme();
+  const defaultThemeType = useSelector(state => state.themeType);
 
   const toggle = e => {
     dispatch(toggleTheme(e.target.value));
--- src/features/theme/index.js
+++ src/features/theme/index.js
@@ -1,2 +1,2 @@
 export { Theme } from './components/Theme';
-export { ToggleTheme } from './components/toggleTheme/ToggleTheme';
+export { ToggleTheme } from './components/ToggleTheme';
