364da6a8ae5037e905536b516e973441e05b0576
shimizacken <shimizacken@gmail.com>
2020-01-12 11:36:12 +0100

    common eslinted

--- src/components/common/button/Button.jsx
+++ src/components/common/button/Button.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import styles from './styles.scss';
 
-export const Button = ({disabled}) => 
-    <button
-        className={styles.searchButton}
-        type='submit'
-        disabled={disabled}
-    >Search</button>;
+export const Button = ({ disabled }) => (
+  <button className={styles.searchButton} type="submit" disabled={disabled}>
+    Search
+  </button>
+);
