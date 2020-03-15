12ec800249eccac2e3af6dc7b2c93b0f5d2820a8
shimizacken <shimizacken@gmail.com>
2020-03-15 22:00:09 +0100

    tests linted

--- cypress/integration/searchWeather.spec.js
+++ cypress/integration/searchWeather.spec.js
@@ -1,31 +1,31 @@
-import { searchWeatherResponse } from "../fixtures/searchResults";
+/* eslint-disable no-undef */
+import { searchWeatherResponse } from '../fixtures/searchResults';
 
-describe('Open default page', () => {
-    
-    beforeEach(() => {
-        cy.visitHomePage('http://localhost:9200/');
+describe('search weather test suite', () => {
+  beforeEach(() => {
+    cy.visitHomePage('http://localhost:9200/');
+  });
+
+  it('should open the home page', () => {
+    cy.contains('weather fetcher');
+  });
+
+  it('type in search box', () => {
+    cy.server();
+    cy.route({
+      method: 'GET',
+      url: 'https://api.openweathermap.org/data/2.5/weather?q=*',
+      status: 200,
+      response: searchWeatherResponse()
     });
-    
-    it('should open the home page', () => {
-        cy.contains('weather fetcher');
-    });
-    
-    it('type in search box', () => {
-        cy.server();
-        cy.route({
-            method: 'GET',
-            url: 'https://api.openweathermap.org/data/2.5/weather?q=*',
-            status: 200,
-            response: searchWeatherResponse()
-        });
 
-        cy.get('[data-cy=search-results]').should('be.not.visible');
-        cy.get('[data-cy=search-weather-button-input]').should('be.enabled');
-        cy.get('[data-cy=search-weather-button]').should('be.enabled');
+    cy.get('[data-cy=search-results]').should('be.not.visible');
+    cy.get('[data-cy=search-weather-button-input]').should('be.enabled');
+    cy.get('[data-cy=search-weather-button]').should('be.enabled');
 
-        cy.get('[data-cy=search-weather-button-input]').type('Drammen');
-        cy.get('[data-cy=search-weather-button]').click();
+    cy.get('[data-cy=search-weather-button-input]').type('Drammen');
+    cy.get('[data-cy=search-weather-button]').click();
 
-        cy.get('[data-cy=search-results]').should('be.visible');
-    });
+    cy.get('[data-cy=search-results]').should('be.visible');
+  });
 });
