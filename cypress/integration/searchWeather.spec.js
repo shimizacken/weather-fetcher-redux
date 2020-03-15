53de3c2d9af8a550c2612810eb4ccc31a0690bb8
shimizacken <shimizacken@gmail.com>
2020-03-15 22:27:42 +0100

    cityName variabled

--- cypress/integration/searchWeather.spec.js
+++ cypress/integration/searchWeather.spec.js
@@ -2,6 +2,8 @@
 import { searchWeatherResponse } from '../fixtures/searchResults';
 
 describe('search weather test suite', () => {
+  const cityName = 'drammen';
+
   beforeEach(() => {
     cy.visitHomePage();
   });
@@ -10,8 +12,7 @@ describe('search weather test suite', () => {
     cy.server();
     cy.route({
       method: 'GET',
-      url:
-        'https://api.openweathermap.org/data/2.5/weather?q=drammen&appid=69002fcf63566f2396643824536f3d9b&units=metric',
+      url: `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=69002fcf63566f2396643824536f3d9b&units=metric`,
       status: 200,
       response: searchWeatherResponse()
     });
@@ -20,7 +21,7 @@ describe('search weather test suite', () => {
     cy.get('[data-cy=search-weather-button-input]').should('be.enabled');
     cy.get('[data-cy=search-weather-button]').should('be.enabled');
 
-    cy.get('[data-cy=search-weather-button-input]').type('Drammen');
+    cy.get('[data-cy=search-weather-button-input]').type(cityName);
     cy.get('[data-cy=search-weather-button]').click();
 
     cy.get('[data-cy=search-results]').should('be.visible');
