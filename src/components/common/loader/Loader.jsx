eb02ca58f7c9b333bf34aa427c981647e6f61035
shimizacken <shimizacken@gmail.com>
2020-03-17 00:11:12 +0100

    Loader typed

--- src/components/common/loader/Loader.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import sun from 'app/assets/png/sun.png';
-import styles from './Loader.scss';
-
-export const Loader = React.memo(() => <img data-cy="loader-image" src={sun} className={styles.rotating} />);
---
+++ src/components/common/loader/Loader.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import sun from 'app/assets/png/sun.png';
+import styles from './Loader.scss';
+
+export const Loader: React.FC = React.memo(() => <img data-cy="loader-image" src={sun} className={styles.rotating} />);
