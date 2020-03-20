7c71ccec9529033ff0754d49429a48e397ed1087
shimizacken <shimizacken@gmail.com>
2020-03-20 21:57:17 +0100

    memoized

--- src/features/layout/components/Header.jsx
+++ src/features/layout/components/Header.jsx
@@ -3,11 +3,11 @@ import sun from 'app/assets/png/sun.png';
 import { ToggleTheme } from 'app/features/theme';
 import styles from './Header.scss';
 
-export const Header = () => (
+export const Header = React.memo(() => (
   <header>
     <h1 className={styles.mainHeader}>
       <img src={sun} /> weather fetcher
     </h1>
     <ToggleTheme />
   </header>
-);
+));
--- src/features/searchWeather/components/ErrorMessage.jsx
+++ src/features/searchWeather/components/ErrorMessage.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './ErrorMessage.scss';
 
-export const ErrorMessage = ({ errorMessage }) => {
+export const ErrorMessage = React.memo(({ errorMessage }) => {
   if (!errorMessage) {
     return null;
   }
 
   return <span className={styles.errorMessage}>{errorMessage}</span>;
-};
+});
 
 ErrorMessage.propTypes = {
   errorMessage: PropTypes.string
