6a7cca313513aa7fc3f6d84a360146da24aa69b0
shimizacken <shimizacken@gmail.com>
2020-03-20 13:40:51 +0100

    story added

--- src/components/common/radioButton/stories/RadioButton.stories.jsx
+++ src/components/common/radioButton/stories/RadioButton.stories.jsx
@@ -3,6 +3,7 @@ import { action } from '@storybook/addon-actions';
 import { withKnobs, boolean } from '@storybook/addon-knobs';
 import { RadioButton } from '../RadioButton';
 import { content } from 'app/stories/decorators/content';
+import styles from './RadioStories.scss';
 
 export default {
   title: 'Inputs/Radio',
@@ -11,15 +12,28 @@ export default {
 };
 
 export const Text = () => (
-  <>
+  <div>
     <RadioButton
       text="Red"
       checked={boolean('checked', true)}
       disabled={boolean('disabled', false)}
       onClick={action('clicked')}
       name="rgb"
+      className={styles.radio}
     />
-    <RadioButton text="Green" disabled={boolean('disabled', false)} onClick={action('clicked')} name="rgb" />
-    <RadioButton text="Blue" disabled={boolean('disabled', false)} onClick={action('clicked')} name="rgb" />
-  </>
+    <RadioButton
+      text="Green"
+      disabled={boolean('disabled', false)}
+      onClick={action('clicked')}
+      name="rgb"
+      className={styles.radio}
+    />
+    <RadioButton
+      text="Blue"
+      disabled={boolean('disabled', false)}
+      onClick={action('clicked')}
+      name="rgb"
+      className={styles.radio}
+    />
+  </div>
 );
