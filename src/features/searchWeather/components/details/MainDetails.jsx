c6eb72eb0edb5b6594c38470a943da4d1aa0e660
shimizacken <shimizacken@gmail.com>
2020-03-21 16:54:51 +0100

    cleanup

---
+++ src/features/searchWeather/components/details/MainDetails.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { buildIconUrl } from 'app/services/openWeatherMap/utils';
+import styles from './WeatherDetailsContainer.scss';
+
+export const MainDetails = React.memo(({ currentWeather, description, temperature, icon }) => (
+  <div className={styles.mainDetailsWrapper}>
+    <div className={styles.iconWrapper}></div>
+    <span>
+      <span className={styles.temperature}>{temperature}</span> °C
+    </span>
+    <div className={styles.currentDescription}>
+      <img src={buildIconUrl(icon)} title={currentWeather} width={50} height={50} /> <h1>{currentWeather}</h1>{' '}
+      <span className={styles.sss}>{description}</span>
+    </div>
+  </div>
+));
+
+MainDetails.propTypes = {
+  currentWeather: PropTypes.string,
+  description: PropTypes.string,
+  temperature: PropTypes.number,
+  icon: PropTypes.string
+};
--- src/features/searchWeather/components/details/WeatherDetailsContainer.jsx
+++ src/features/searchWeather/components/details/WeatherDetailsContainer.jsx
@@ -2,10 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
 import isEmpty from 'lodash/isEmpty';
-import { buildIconUrl } from 'app/services/openWeatherMap/utils';
-import { Details } from './Details';
 import { selectSearchResult } from '../../state/weatherSelectors';
 import { Title } from './Title';
+import { MainDetails } from './MainDetails';
 import styles from './WeatherDetailsContainer.scss';
 
 export const WeatherDetailsContainer = () => {
@@ -16,24 +15,18 @@ export const WeatherDetailsContainer = () => {
   }
 
   const city = weather.weather?.[0];
-  const { main, wind, clouds } = weather;
+  const { main } = weather;
 
   return (
     <div className={styles.detailsWrapper}>
       <div className={styles.cityName}>
         <Title name={weather.name} country={weather.sys?.country} />
       </div>
-      <div className={styles.iconWrapper}>
-        <img src={buildIconUrl(city?.icon)} title={city?.main} />
-      </div>
-      <Details
-        cityName={city?.main}
-        description={city?.description}
+      <MainDetails
+        currentWeather={city?.main}
         temperature={main.temp}
-        pressure={main.pressure}
-        humidity={main.humidity}
-        speed={wind.speed}
-        cloudsAll={clouds.all}
+        description={city?.description}
+        icon={city?.icon}
       />
     </div>
   );
@@ -42,8 +35,6 @@ export const WeatherDetailsContainer = () => {
 WeatherDetailsContainer.propTypes = {
   weatherData: PropTypes.shape({
     name: PropTypes.string,
-    sys: PropTypes.shape({})
+    sys: PropTypes.shape()
   })
 };
-
-WeatherDetailsContainer.defaultProps = {};
