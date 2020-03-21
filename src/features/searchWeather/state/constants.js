0a95ed6332d91d6a886fd79505a88d0185810c72
shimizacken <shimizacken@gmail.com>
2020-03-21 21:08:10 +0100

    cleanup

--- src/features/searchWeather/state/constants.js
+++ src/features/searchWeather/state/constants.js
@@ -2,7 +2,6 @@ import { pendingType, successType, errorType } from 'app/state/constants';
 
 export const FETCH_WEATHER = 'weather/fetch';
 export const SET_WEATHER = 'weather/set';
-export const SET_WEATHER_END = 'weather/set/end';
 
 export const FETCH_WEATHER_PENDING = pendingType(FETCH_WEATHER);
 export const FETCH_WEATHER_SUCCESS = successType(FETCH_WEATHER);
--- src/features/searchWeather/state/weatherActions.js
+++ src/features/searchWeather/state/weatherActions.js
@@ -1,11 +1,4 @@
-import {
-  SET_WEATHER,
-  SET_WEATHER_END,
-  FETCH_WEATHER_PENDING,
-  FETCH_WEATHER_ERROR,
-  FETCH_WEATHER_SUCCESS,
-  FETCH_WEATHER
-} from './constants';
+import { SET_WEATHER, FETCH_WEATHER_PENDING, FETCH_WEATHER } from './constants';
 
 export const searchWeather = promiseFunc => ({
   type: FETCH_WEATHER,
@@ -17,40 +10,6 @@ export const setWeather = weather => ({
   weather
 });
 
-export const setWeatherEnd = {
-  type: SET_WEATHER_END
-};
-
 export const fetchWeatherPending = {
   type: FETCH_WEATHER_PENDING
 };
-
-export const fetchWeatherSuccess = weather => ({
-  type: FETCH_WEATHER_SUCCESS,
-  weather
-});
-
-export const fetchWeatherError = {
-  type: FETCH_WEATHER_ERROR
-};
-
-// export const fetchWeather = url => dispatch => {
-//   dispatch(fetchWeatherPending);
-//   dispatch(setWeatherFetchFlag(true));
-
-//   return fetch(url)
-//     .then(response => response.json())
-//     .then(data => {
-//       if (data && data.cod !== 200) {
-//         throw data.message;
-//       }
-
-//       dispatch(setWeather(data));
-//       dispatch(setWeatherEnd);
-//     })
-//     .catch(error => {
-//       console.error(error);
-//       dispatch(fetchWeatherError);
-//     })
-//     .finally(() => dispatch(setWeatherFetchFlag(false)));
-// };
