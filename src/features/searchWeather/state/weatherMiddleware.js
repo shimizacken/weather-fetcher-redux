3ad05b9fbe7b5b9edee2e380eb20d6dfbb3e81a0
shimizacken <shimizacken@gmail.com>
2020-03-24 10:09:29 +0100

    ResponseCode added

--- src/features/searchWeather/state/weatherMiddleware.js
+++ src/features/searchWeather/state/weatherMiddleware.js
@@ -1,15 +1,16 @@
 import { FETCH_WEATHER_SUCCESS } from './constants';
-import { setWeather, fetchWeatherError } from './weatherActions';
+import { ResponseCode } from 'app/services/net/responseCode';
 import { setHistory } from 'app/features/searchHistory/state/searchHistoryActions';
 import { selectMetricType } from 'app/features/metricType';
 import { getUnitNameByValue } from 'app/services/openWeatherMap/units';
 import { historyListLocalStorageKey } from 'app/features/searchHistory/bll/historyListStorageKey';
 import { mapWeatherResponse } from '../bll/mapWeatherResponse';
+import { setWeather, fetchWeatherError } from './weatherActions';
 import { buildHistoryItem } from './buildHistoryItem';
 
 export const weatherMiddleware = ({ dispatch, getState }) => (next) => (action) => {
   if (action.type === FETCH_WEATHER_SUCCESS) {
-    if (action?.payload?.cod === '404') {
+    if (action?.payload?.cod === ResponseCode.NOT_FOUND) {
       dispatch(fetchWeatherError(action.payload));
     } else {
       const mappedWeather = mapWeatherResponse(action.payload);
