e279c0442d40506bc37470bb60fd9c735f83dc40
shimizacken <shimizacken@gmail.com>
2020-04-02 20:12:48 +0200

    linted

--- src/services/net/fetch.js
+++ src/services/net/fetch.js
@@ -1,5 +1,5 @@
-export const request = url => {
-  return fetch(url).then(response => {
+export const request = (url) => {
+  return fetch(url).then((response) => {
     return response.json();
   });
 };
--- src/services/openweathermap/utils.js
+++ src/services/openweathermap/utils.js
@@ -1,8 +1,8 @@
 export const buildApiUrl = (token, unit) => {
   unit = unit ? `&units=${unit}` : '';
 
-  return cityName =>
+  return (cityName) =>
     `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(cityName)}&appid=${token}${unit}`;
 };
 
-export const buildIconUrl = icon => `http://openweathermap.org/img/w/${icon}.png`;
+export const buildIconUrl = (icon) => `http://openweathermap.org/img/w/${icon}.png`;
