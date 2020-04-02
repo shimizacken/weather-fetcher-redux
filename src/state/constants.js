7425c21fff0d4e8f4ab79a7078a9b8f1f6ea5d64
shimizacken <shimizacken@gmail.com>
2020-04-02 20:09:28 +0200

    linted

--- src/state/constants.js
+++ src/state/constants.js
@@ -9,14 +9,14 @@ export const error = 'error';
 /**
  * Append '_pending' suffix to given action type.
  */
-export const pendingType = actionType => `${actionType}_${pending}`;
+export const pendingType = (actionType) => `${actionType}_${pending}`;
 
 /**
  * Append '_success' suffix to given action type.
  */
-export const successType = actionType => `${actionType}_${success}`;
+export const successType = (actionType) => `${actionType}_${success}`;
 
 /**
  * Append '_error' suffix to given action type.
  */
-export const errorType = actionType => `${actionType}_${error}`;
+export const errorType = (actionType) => `${actionType}_${error}`;
