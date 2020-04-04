0197ff6121062d088a1e8d087f38e6695ae532c0
shimizacken <shimizacken@gmail.com>
2020-04-04 15:21:38 +0200

    history added

--- src/features/layout/components/RootContainer.jsx
+++ src/features/layout/components/RootContainer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import { Header } from 'app/features/layout';
 import { WeatherContainer } from 'app/features/searchWeather';
 import { SearchHistoryContainer } from 'app/features/searchHistory';
@@ -7,7 +8,18 @@ import styles from './RootContainer.scss';
 export const RootContainer = () => (
   <div className={styles.root}>
     <Header />
-    <SearchHistoryContainer />
-    <WeatherContainer />
+    <Switch>
+      <Route
+        path="/"
+        render={() => (
+          <>
+            <SearchHistoryContainer />
+            <WeatherContainer />
+          </>
+        )}
+        exact
+      />
+      <Route path="/history" render={() => <SearchHistoryContainer />} exact />
+    </Switch>
   </div>
 );
