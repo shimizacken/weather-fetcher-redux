92d50f3ba398e83f1a09621737e08919c783650e
shimizacken <shimizacken@gmail.com>
2020-04-13 19:05:44 +0200

    new logo

--- src/features/layout/components/Header.jsx
+++ src/features/layout/components/Header.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import sun from 'app/assets/png/sun.png';
+import sun from 'app/assets/png/logo.png';
 import { ToggleTheme } from 'app/features/theme';
 import styles from './Header.scss';
 
 export const Header = React.memo(() => (
   <header>
     <h1 className={styles.mainHeader}>
-      <img src={sun} /> weather fetcher
+      <img src={sun} />
     </h1>
     <ToggleTheme />
   </header>
