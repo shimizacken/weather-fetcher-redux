a4891692eea55387ab756e7746229f6d2220f1d1
shimizacken <shimizacken@gmail.com>
2020-04-22 10:50:36 +0200

    rowRenderer added

--- src/components/common/table/Table.jsx
+++ src/components/common/table/Table.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import { Rows } from './Rows';
 import { Column } from './Column';
 import style from './Table.module.scss';
 
-export const Table = ({ headerItems, rowItems, columnClassName }) => {
+export const Table = ({ headerItems, items, tableClassName, columnClassName, rowRenderer }) => {
   const width = 100 / headerItems?.length;
+  const rowItems = rowRenderer(items);
 
   return (
-    <div className={style.tableWrapper}>
+    <div className={classNames(style.table, tableClassName)}>
       <div className={style.header}>
         {headerItems?.map(title => (
           <Column key={title} value={title} width={width} className={columnClassName} />
@@ -25,12 +27,17 @@ export const Table = ({ headerItems, rowItems, columnClassName }) => {
 
 Table.propTypes = {
   headerItems: PropTypes.arrayOf(PropTypes.string),
-  rowItems: PropTypes.arrayOf(PropTypes.shape()),
+  items: PropTypes.arrayOf(PropTypes.shape()),
+  rowRenderer: PropTypes.func.isRequired,
+  tableClassName: PropTypes.string,
+  rowClassName: PropTypes.string,
   columnClassName: PropTypes.string
 };
 
 Table.defaultProps = {
   headerItems: undefined,
   rowItems: undefined,
+  tableClassName: undefined,
+  rowClassName: undefined,
   columnClassName: undefined
 };
--- src/features/searchHistory/components/historyTable/HistoryTable.jsx
+++ src/features/searchHistory/components/historyTable/HistoryTable.jsx
@@ -2,27 +2,24 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Table } from 'app/components/common';
 import { RemoveHistoryModalContainer } from '../RemoveHistoryModalContainer';
-import { buildIconUrl } from '../../../searchWeather';
+import { historyTableRowRenderer } from '../../bll/historyTableRowRenderer';
 import style from './HistoryTable.module.scss';
 
 export const HistoryTable = ({ items }) => {
   const headerItems = ['City Name', 'description', 'Temperature', 'Icon', 'Date'];
 
-  const rowItems = items?.map(item => {
-    return {
-      id: item.id,
-      name: `${item.name}, ${item.country}`,
-      description: item.description,
-      temperature: `${item.temperature}${item.tempType.symbol}`,
-      icon: <img src={buildIconUrl(item.icon)} title={item.description} />,
-      date: new Date(item.date).toLocaleString()
-    };
-  });
-
   return (
     <div className={style.historyTableWrapper} data-cy="history-table">
       <RemoveHistoryModalContainer />
-      <Table headerItems={headerItems} rowItems={rowItems} columnClassName={style.column} />
+      <div>
+        <Table
+          headerItems={headerItems}
+          items={items}
+          tableClassName={style.table}
+          columnClassName={style.column}
+          rowRenderer={historyTableRowRenderer}
+        />
+      </div>
     </div>
   );
 };
