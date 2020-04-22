a7f2b41bb1db7b0605910aca4d7318e4f67322a5
shimizacken <shimizacken@gmail.com>
2020-04-22 12:32:38 +0200

    minor changes

--- src/features/searchWeather/state/weatherSelectors.js
+++ src/features/searchWeather/state/weatherSelectors.js
@@ -1,7 +1,7 @@
-export const selectSearchResult = (state) => state?.weatherSearch?.searchResult?.weather;
+export const selectSearchResult = state => state?.weatherSearch?.searchResult?.weather;
 
-export const selectIsSearchWeatherFetching = (state) => state?.weatherSearch?.fetchingWeatherApiState?.isFetching;
+export const selectIsSearchWeatherFetching = state => state?.weatherSearch?.fetchingWeatherApiState?.isFetching;
 
-export const selectIsSearchWeatherFailed = (state) => state?.weatherSearch?.fetchingWeatherApiState?.isError;
+export const selectIsSearchWeatherFailed = state => state?.weatherSearch?.fetchingWeatherApiState?.isError;
 
-export const selectIsSearchWeatherErrorMessage = (state) => state?.weatherSearch?.fetchingWeatherApiState?.errorMessage;
+export const selectIsSearchWeatherErrorMessage = state => state?.weatherSearch?.fetchingWeatherApiState?.errorMessage;
