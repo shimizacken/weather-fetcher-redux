748add5bce8416eece8a3bff5bdf220cfe235045
shimizacken <shimizacken@gmail.com>
2020-08-13 23:17:48 +0200

    refactor: to ts

---
+++ src/components/common/button/buttonTypes.ts
@@ -0,0 +1,9 @@
+export type ButtonTypes = {
+  [key: string]: string;
+};
+
+export enum ButtonType {
+  BUTTON = 'button',
+  RESET = 'reset',
+  SUBMIT = 'submit'
+}
--- src/components/common/button/stories/Button.stories.tsx
+++ src/components/common/button/stories/Button.stories.tsx
@@ -4,7 +4,7 @@ import { withKnobs, text, boolean, select } from '@storybook/addon-knobs';
 import { getSection, Parent, Child } from 'app/stories/sections';
 import { content } from 'app/stories/decorators';
 import { Button } from '../Button';
-import { ButtonTypes } from '../buttonTypes';
+import { ButtonType } from '../buttonTypes';
 
 export default {
   title: getSection(Parent.inputs, Child.button),
@@ -16,7 +16,7 @@ export const Default = () => (
   <Button
     text={text('text', 'Hello Button')}
     disabled={boolean('disabled', false)}
-    buttonType={select('ButtonTypes', ButtonTypes, ButtonTypes.BUTTON)}
+    buttonType={select('ButtonTypes', ButtonType, ButtonType.BUTTON)}
     onClick={action('clicked')}
   />
 );
