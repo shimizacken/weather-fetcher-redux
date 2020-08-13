539e1258b6a85bb3182174641e7d1c3df8650ac4
shimizacken <shimizacken@gmail.com>
2020-08-13 23:20:34 +0200

    chore: default added

--- src/components/common/button/BackButton.tsx
+++ src/components/common/button/BackButton.tsx
@@ -8,7 +8,7 @@ export const BackButton: React.FC<React.ComponentProps<'a'> & {
   disabled?: boolean;
   to?: string;
   dataCy?: string;
-}> = React.memo(({ text, disabled, to, className, dataCy }) => (
+}> = React.memo(({ text, disabled = false, to, className, dataCy }) => (
   <Link className={classNames(styles.backButton, className)} disabled={disabled} to={to || '/'} data-cy={dataCy}>
     {text || '<'}
   </Link>
