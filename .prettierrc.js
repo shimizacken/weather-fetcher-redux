8fd314bf668b149520407335f06bd569f24b6054
shimizacken <shimizacken@gmail.com>
2020-08-13 22:56:48 +0200

    chore: overrides

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,17 @@
 module.exports = {
-    trailingComma: "none",
-    printWidth: 120,
-    tabWidth: 2,
-    singleQuote: true,
-    useTabs: false,
-    bracketSpacing: true
+  trailingComma: 'none',
+  printWidth: 120,
+  tabWidth: 2,
+  singleQuote: true,
+  useTabs: false,
+  bracketSpacing: true,
+
+  overrides: [
+    {
+      files: ['*.scss.d.ts'],
+      options: {
+        tabWidth: 2
+      }
+    }
+  ]
 };
