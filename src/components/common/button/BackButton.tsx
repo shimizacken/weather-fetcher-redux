92a53747159e26fc50c4cc512b85ed249f6c09d8
shimizacken <shimizacken@gmail.com>
2020-08-13 23:09:07 +0200

    refactor: to ts

---
+++ src/components/common/button/BackButton.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import classNames from 'classnames';
+import styles from './BackButton.module.scss';
+
+export const BackButton: React.FC<React.ComponentProps<'a'> & {
+  text?: string;
+  disabled?: boolean;
+  to?: string;
+  dataCy?: string;
+}> = React.memo(({ text, disabled, to, className, dataCy }) => (
+  <Link className={classNames(styles.backButton, className)} disabled={disabled} to={to || '/'} data-cy={dataCy}>
+    {text || '<'}
+  </Link>
+));
