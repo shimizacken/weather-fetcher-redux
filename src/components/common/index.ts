1b18e9f1ef473e0a99b91d8195e4cb6e378e2b4c
shimizacken <shimizacken@gmail.com>
2020-08-16 08:28:06 +0200

    chore: typings

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,8 +1,10 @@
 export {Button} from './button/Button';
+export {ButtonType} from './button/buttonType';
 export {SearchBox} from './searchBox/SearchBox';
 export {RadioButton} from './radioButton/RadioButton';
 export {Loader} from './loader/Loader';
 export {Table, Row, Column} from './table';
 export {Modal, ConfirmationModal} from './modal';
-export {Logo} from './logo/Logo';
 export {BackButton} from './button';
+export {TextInput} from './textInput/TextInput';
+export {Logo} from './logo/Logo';
--- src/components/common/searchBox/SearchBox.tsx
+++ src/components/common/searchBox/SearchBox.tsx
@@ -1,28 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { TextInput } from 'app/components/common/textInput/TextInput';
-import { Button, ButtonType } from 'app/components/common/button';
+import {TextInput, Button, ButtonType} from 'app/components/common';
 import styles from './SearchBox.scss';
 
-export const SearchBox = ({ value, onChange, disabled }) => {
-  return (
-    <div className={styles.searchBoxWrapper}>
-      <TextInput
-        value={value}
-        onChange={onChange}
-        placeholder="Type city name and hit the enter..."
-        disabled={disabled}
-        dataCy="search-weather-button-input"
-      />
-      <div className={styles.buttonWrapper}>
-        <Button text="Search" disabled={disabled} buttonType={ButtonType.SUBMIT} dataCy="search-weather-button" />
-      </div>
-    </div>
-  );
+export const SearchBox = ({value, onChange, disabled}) => {
+    return (
+        <div className={styles.searchBoxWrapper}>
+            <TextInput
+                value={value}
+                onChange={onChange}
+                placeholder="Type city name and hit the enter..."
+                disabled={disabled}
+                dataCy="search-weather-button-input"
+            />
+            <div className={styles.buttonWrapper}>
+                <Button
+                    text="Search"
+                    disabled={disabled}
+                    buttonType={ButtonType.SUBMIT}
+                    dataCy="search-weather-button"
+                />
+            </div>
+        </div>
+    );
 };
 
 SearchBox.propTypes = {
-  value: PropTypes.string.isRequired,
-  onChange: PropTypes.func.isRequired,
-  disabled: PropTypes.bool.isRequired
+    value: PropTypes.string.isRequired,
+    onChange: PropTypes.func.isRequired,
+    disabled: PropTypes.bool.isRequired,
 };
